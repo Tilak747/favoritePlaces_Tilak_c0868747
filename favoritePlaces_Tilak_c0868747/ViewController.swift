4170e59d481276d5f5633f0e53471e8387b0e693
Tilak <c0868747@mylambton.ca>
2023-01-24 18:05:58 -0500

    Initial Commit

---
+++ favoritePlaces_Tilak_c0868747/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favoritePlaces_Tilak_c0868747
+//
+//  Created by Tilak Acharya on 2023-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
